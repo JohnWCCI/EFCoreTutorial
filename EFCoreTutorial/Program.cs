6cb9fc269c9b752fb0843cf7f799a62f6165fd44
John Moseley <johnmoseley@Wecancodeit.org>
2023-02-06 00:33:24 -0500

    Added Controllers

--- EFCoreTutorial/Program.cs
+++ EFCoreTutorial/Program.cs
@@ -1,7 +1,9 @@
+using EFCoreTutorial.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<MusicContext>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
